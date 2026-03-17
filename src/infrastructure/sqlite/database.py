ac3ba6a95b9547dce53a2621790debe13d7df365
Gvozdarev Kirill <gvozdarevkirill@email.com>
2026-03-17 20:42:35 +0400

    Creating a connection to the database

---
+++ src/infrastructure/sqlite/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# Строка подключения
+SQLALCHEMY_DATABASE_URL = "sqlite:///...../fastapiDB.db"
+# Создание движка (в аргументе разрешаем многопоточность)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+# Создание класса-построителя для создания объекта сессии базы данных при помощи функции-фабрики
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+# Создание базовой модели для таблиц бд
+Base = declarative_base()
+# Определяем зависимость
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
